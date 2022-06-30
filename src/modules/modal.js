445d218ea379f57df57f832129ac7f057533ff27
Dmitry Korochkin <abramslost@gmail.com>
2022-06-30 18:14:18 +0300

    fix animation

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -14,12 +14,12 @@ const modal = () => {
                     return 1;
                 },
                 draw(progress) {
-                    modal.style.opacity = progress
-                    modal.style.display = 'block' 
+                    modal.style.opacity = progress;
+                    modal.style.display = 'block' ;
                 } 
             });
         } else {
-            modal.style.display = 'block' 
+            modal.style.display = 'block';
         }
     }
    
