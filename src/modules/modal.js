55652e1426ce847f18d29480bdc93bf0d452c07e
Dmitry Korochkin <abramslost@gmail.com>
2022-06-30 18:12:53 +0300

    Добавил анимацию

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,18 +1,35 @@
+import animate from './animate'
+
 const modal = () => {
     const buttons = document.querySelectorAll('.popup-btn');
     const modal = document.querySelector('.popup');
     const popupClose = modal.querySelector('.popup-close');
    
+
+    const animateModal = () => {
+        if (screen.width > 768) {
+            animate({
+                duration: 700,
+                timing(timeFraction) {
+                    return 1;
+                },
+                draw(progress) {
+                    modal.style.opacity = progress
+                    modal.style.display = 'block' 
+                } 
+            });
+        } else {
+            modal.style.display = 'block' 
+        }
+    }
    
     
 
-    console.log(popupClose)
+    
 
     buttons.forEach(btn => { 
         btn.addEventListener('click', () => {
-            modal.style.display = 'block';  
-            
-
+            animateModal()
     
         })
     })
