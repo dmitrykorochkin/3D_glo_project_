383df9efafb221e01392c0fb62f2fefc31d06d08
Dmitry Korochkin <abramslost@gmail.com>
2022-07-02 15:02:46 +0300

    add input

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@
 import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
+import input from './modules/input';
+
 
 
 timer('31 july 2022');
 menu();
 modal();
+input();
+
+
 
 
