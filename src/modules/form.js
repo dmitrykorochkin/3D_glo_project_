861603c092b9cd181b30a60c7f4113ff635d1bd2
Dmitry Korochkin <abramslost@gmail.com>
2022-07-08 11:33:44 +0300

    fix form

--- src/modules/form.js
+++ src/modules/form.js
@@ -1,37 +1,37 @@
 const form = () => {
 
-const inputAll = document.querySelectorAll('input[type="text"]');
-const inputMessange = document.querySelector('input[placeholder="Ваше сообщение"]');
-const inputEmail = document.querySelectorAll('input[type="email"]');
-const inputTel = document.querySelectorAll('input[type="tel"]');
-
-    inputAll.forEach(element => {
-        element.addEventListener('input', () => {
-        element.value = element.value.replace(/[^а-я-\s]/gi,'');
-        
-    })
-})
-
-
-    inputMessange.addEventListener('input', () => {
-        inputMessange.value = inputMessange.value.replace(/[^а-я-\s]/gi,'');
-        
-    })
-
-    inputEmail.forEach(element => {
-        element.addEventListener('input', () => {
-        element.value = element.value.replace(/[^\d\w\-\@\_\.\!\~\*\']+/gi, '');
-        
-    })
-
-    inputTel.forEach(element => {
-        element.addEventListener('input',() =>{
-          element.value = element.value.replace(/[^\d\-()']+/gi, "")
-        })
-      });
-})
-
-
-}
-
-export default form;
+  const inputName = document.querySelectorAll('input[name = user_name]');
+  const inputMessange = document.querySelector('input[name = user_message]');
+  const inputEmail = document.querySelectorAll('input[name = user_email]');
+  const inputTel = document.querySelectorAll('input[name = user_phone]');
+  
+      inputName.forEach(element => {
+          element.addEventListener('input', () => {
+          element.value = element.value.replace(/[^а-я-\s]/gi,'');
+          
+      })
+  })
+  
+  
+      inputMessange.addEventListener('input', () => {
+          inputMessange.value = inputMessange.value.replace(/[^а-я-\s]/gi,'');
+          
+      })
+  
+      inputEmail.forEach(element => {
+          element.addEventListener('input', () => {
+          element.value = element.value.replace(/[^\d\w\-\@\_\.\!\~\*\']+/gi, '');
+          
+      })
+  
+      inputTel.forEach(element => {
+          element.addEventListener('input',() =>{
+            element.value = element.value.replace(/[^\d\-()']+/gi, "")
+          })
+        });
+  })
+  
+  
+  }
+  
+  export default form;
