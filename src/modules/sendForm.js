6fd74d75a6b095fc68c26f99d091f725e48d3231
Dmitry Korochkin <abramslost@gmail.com>
2022-07-18 14:21:50 +0300

    fix validation

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,3 +1,108 @@
+
+
+// const sendForm = ({idForm, somElem = []}) => {
+
+//     const form = document.getElementById(idForm);
+//     const statusBlock = document.createElement('div');
+//     const loadText = 'Загрузка...';
+//     const errorText = 'Ошибка...';
+//     const successText = 'Спасибо, наш менеджер свяжется с вами!';
+
+//     const validate = (list) => {
+//         let success = true;
+//         return success;
+
+        
+//     }
+
+
+//     const sendData = (data) => {
+//         return fetch('https://jsonplaceholder.typicode.com/posts', {
+//             method: 'POST',
+//             body: JSON.stringify(data),
+//             headers: { 'Content-Type': 'application/json'}
+//         }).then(res => res.json());
+//     }
+
+//     const submitForm = () => {
+//         const formElements = form.querySelectorAll('input');
+//         const formData = new FormData(form);
+//         const formBody = {};
+
+//         statusBlock.textContent = loadText;
+//         form.append(statusBlock);
+
+
+//         formData.forEach((val, key) => {
+//             formBody[key] = val
+//         });
+
+//         somElem.forEach(elem => {
+//             const element = document.getElementById(elem.id)
+
+           
+//             if (elem.type === 'block') {
+//                 formBody[elem.id] = element.textContent;
+//             } else if (elem.type === 'input') {
+//                 formBody[elem.id] = element.value;
+//             }
+
+//         })
+
+//         if (validate(formElements)) {
+//             sendData(formBody)
+//                 .then(data => {
+//                     statusBlock.textContent = successText;
+//                     formElements.forEach(input => {   // после отправки очищаем input
+//                         input.value = '';
+//                 }) 
+//             })
+
+//             .catch(error => {
+
+//                 statusBlock.textContent = errorText;
+//             })
+//         } else {
+//             alert('Данные не валидны');
+//         }
+//     }
+
+//     try {
+//         if (!form){
+//             throw new Error('Ошибка в форме');
+//         }
+//         form.addEventListener('submit', (event) => {
+//             event.preventDefault();
+    
+//             submitForm();
+//         });
+
+//     } catch(error) {
+//         console.log(error.message);
+//     }
+
+    
+        
+    
+    
+// }
+
+// export default sendForm;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 "use strict";
 
 const sendForm = ({ idForm, somElem = [] }) => {
@@ -103,9 +208,9 @@ const sendForm = ({ idForm, somElem = [] }) => {
       form.append(statusBlock);
       statusBlock.textContent = textError;
 
-      // formElements.forEach((input) => {
-      //   input.value = "";
-      // });
+      formElements.forEach((input) => {
+        input.value = "";
+      });
       setTimeout(() => {
         statusBlock.textContent = "";
       }, 2000);
