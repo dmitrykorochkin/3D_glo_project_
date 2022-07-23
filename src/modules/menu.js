e244b606dda18cb4c66563eb5e1f3df86f5a36c8
Dmitry <51984125+dmitrykorochkin@users.noreply.github.com>
2022-07-23 08:49:55 +0300

    fix menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,22 +1,46 @@
 
+// const menu = () => {
+
+//     const menu = document.querySelector('menu');
+	
+
+// 	const handleMenu = (e) => {
+// 		e.preventDefault();
+// 		menu.classList.toggle('active-menu');
+// 	};
+// 	document.body.addEventListener('click', e => {
+		
+// 		if (e.target.closest('.menu') || e.target.matches('menu a')) {
+// 			handleMenu(e);
+// 		} else if (!e.target.closest('menu')) {
+// 			if (menu.classList.contains('active-menu')) {
+// 				handleMenu(e);
+// 			}
+// 		}
+// 	});
+
+// }
+
+// export default menu;
+
+
+
 const menu = () => {
-
     const menu = document.querySelector('menu');
+    const closeBtn = menu.querySelector('.close-btn');
 
-	const handleMenu = () => {
-		menu.classList.toggle('active-menu');
-	};
-	document.body.addEventListener('click', e => {
-		e.preventDefault()
-		if (e.target.closest('.menu') || e.target.matches('menu a')) {
-			handleMenu();
-		} else if (!e.target.closest('menu')) {
-			if (menu.classList.contains('active-menu')) {
-				handleMenu();
-			}
-		}
-	});
+    document.addEventListener('click', e => {
+		
+        if (e.target.closest('.menu')) {
+            menu.classList.add('active-menu');
+        } else if (e.target.closest('a') || !e.target.closest('menu')) {
+            menu.classList.remove('active-menu');
+        }
 
-}
+        if (e.target === closeBtn) e.preventDefault();
+    });
+};
 
-export default menu;
+export default menu
+
+
